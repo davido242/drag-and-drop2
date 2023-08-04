dc2ca19f33b3acb7c8f85dab81a23e9a4fb92aa5
Monday_David_Sarka <david_md@hamari.agency>
2023-08-04 13:26:51 +0100

    Added layout and styles

---
+++ main.js
@@ -0,0 +1,6 @@
+const draggables = document.querySelectorAll(".draggable");
+const container = document.querySelectorAll(".container");
+
+draggables.forEach(draggable => {
+  console.log(draggable);
+})
